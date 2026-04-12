37d5f0b565f42128b1c84c2839af6bec2f0c9bdb
Fukuya <fukuyarieru@gmail.com>
2026-04-12 23:18:20 +0300

    changing to using of raw Value as the sending messages

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use crate::client::Client;
+use crate::tcp_node::TcpNode;
 
-mod client;
+mod tcp_node;
 mod test;
 
 fn main() {
-    let mut client = Client::new();
+    let mut client = TcpNode::new();
     client.start_handling();
-    while let Ok(message) = client.receiver_of_messages_from_connections.recv() {
-        match message {
-            client::Message::Command(command) => {}
-            client::Message::Text(text) => println!("{}", text),
-        }
-    }
+    while let Ok(message) = client.receiver_of_messages_from_connections.recv() {}
 }
--- src/tcp_node.rs
+++ src/tcp_node.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::{
     io::{BufRead, BufReader, BufWriter, Result, Write},
     net::{TcpListener, TcpStream},
@@ -9,38 +9,54 @@ use std::{
     thread::{JoinHandle, spawn},
 };
 
-pub struct Client {
+pub struct TcpNode {
     pub connections: Arc<Mutex<Vec<Connection>>>,
     tcp_listener: TcpListener,
     /// connections send messages here
-    pub receiver_of_messages_from_connections: Receiver<Message>,
+    pub receiver_of_messages_from_connections: Receiver<Value>,
     /// sender given to each conenction to send the client new messages
-    sender_to_client: Sender<Message>,
+    sender_to_client: Sender<Value>,
     /// vec storing each sender of every connection
-    senders_to_connections: Arc<Mutex<Vec<Sender<Message>>>>,
+    senders_to_connections: Arc<Mutex<Vec<Sender<Value>>>>,
     /// thread accepting new connections
     new_connections_handle: Option<JoinHandle<()>>,
     /// sender to send messages to connections
-    pub sender_to_connections: Sender<Message>,
+    pub sender_to_connections: Sender<Value>,
     /// thread sending messages of the client to all connections
     sender_to_connections_handle: Option<JoinHandle<()>>,
 }
 
-impl Client {
+impl TcpNode {
     /// method to create a new client.
     /// said client needs to be "started" to accept connections
     pub fn new() -> Self {
         let (sender, receiver) = channel();
-        let (_replace_sender, _) = channel();
+        let (sender_to_connections, receiver_of_sender_to_connections) = channel();
+        let senders_to_connections = Arc::new(Mutex::new(Vec::new()));
+        let senders_to_connections_clone = senders_to_connections.clone();
+        let tcp_listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        #[cfg(debug_assertions)]
+        println!(
+            "NODE ADDRESS -> {}",
+            tcp_listener.local_addr().unwrap().to_string()
+        );
         Self {
             connections: Arc::new(Mutex::new(Vec::new())),
-            tcp_listener: TcpListener::bind("127.0.0.1:8080").unwrap(),
+            tcp_listener: tcp_listener,
             receiver_of_messages_from_connections: receiver,
             sender_to_client: sender,
-            senders_to_connections: Arc::new(Mutex::new(Vec::new())),
+            senders_to_connections: senders_to_connections,
             new_connections_handle: None,
-            sender_to_connections: _replace_sender,
-            sender_to_connections_handle: None,
+            // CONSIDER MOVING THIS BACK TO "start_handling" (on the grounds of not starting a thread immeadiatly upon creating a TcpNode)
+            sender_to_connections: sender_to_connections,
+            sender_to_connections_handle: Some(spawn(move || {
+                while let Ok(message) = receiver_of_sender_to_connections.recv() {
+                    for sender_to_connection in senders_to_connections_clone.lock().unwrap().iter()
+                    {
+                        sender_to_connection.send(message.clone()).unwrap();
+                    }
+                }
+            })),
         }
     }
 
@@ -74,22 +90,12 @@ impl Client {
         let senders_to_connections = self.senders_to_connections.clone();
         self.new_connections_handle = Some(spawn(move || {
             for stream in tcp_listener.incoming() {
-                let (s, r) = channel::<Message>();
+                let (s, r) = channel::<Value>();
                 let c = Connection::new(stream.unwrap(), sender_to_client.clone(), r);
                 #[cfg(debug_assertions)]
                 println!("CONNECTION -> {:?}", c);
                 connections.lock().unwrap().push(c);
                 senders_to_connections.lock().unwrap().push(s);
-            }
-        }));
-        let senders_to_connections = self.senders_to_connections.clone();
-        let (main_sender_to_connections, receiver_of_sender_to_connections) = channel::<Message>();
-        self.sender_to_connections = main_sender_to_connections;
-        self.sender_to_connections_handle = Some(spawn(move || {
-            while let Ok(message) = receiver_of_sender_to_connections.recv() {
-                for sender_to_connection in senders_to_connections.lock().unwrap().iter() {
-                    sender_to_connection.send(message.clone()).unwrap();
-                }
             }
         }));
     }
@@ -113,9 +119,9 @@ impl Connection {
     pub fn new(
         stream: TcpStream,
         // own client may recieve message, these are sent to a receiver on the client through this sender the Connection got
-        sender_to_client: Sender<Message>,
+        sender_to_client: Sender<Value>,
         // own client may send messages, which are recieved by the Connection through this receiver
-        receiver_of_connection: Receiver<Message>,
+        receiver_of_connection: Receiver<Value>,
     ) -> Connection {
         let incoming_address = stream.peer_addr().unwrap().to_string();
         let reader = BufReader::new(stream.try_clone().unwrap());
@@ -125,7 +131,7 @@ impl Connection {
             read_handle: spawn(move || {
                 for line in reader.lines() {
                     let json = line.expect("Client Disconnected");
-                    let message = serde_json::from_str::<Message>(&json).unwrap();
+                    let message = serde_json::from_str::<Value>(&json).unwrap();
                     // sender_clone.send(message).unwrap();
                     sender_to_client.send(message).unwrap();
                 }
@@ -145,13 +151,13 @@ impl Connection {
     }
 }
 
-///  Example:
-/// * Command - starts with /
-/// * Text - anything that isn't a command
-/// ---
-/// Can be whatever you want, handle this as you may please
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum Message {
-    Command(String),
-    Text(String),
-}
+// ///  Example:
+// /// * Command - starts with /
+// /// * Text - anything that isn't a command
+// /// ---
+// /// Can be whatever you want, handle this as you may please
+// #[derive(Serialize, Deserialize, Debug, Clone)]
+// pub enum Value {
+//     Command(String),
+//     Text(String),
+// }
